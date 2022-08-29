ceb2bdb8c6f785ea80a8067b59c8908eb35d3b8e
unohayato <mamidori1p1p@gmail.com>
2022-08-29 21:36:53 +0900

    createView

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 # ListViewとDetailViewを取り込み
 from django.views.generic import ListView, DetailView
 from .models import Post
-
+from django.views.generic.edit import CreateView
 # ListViewは一覧を簡単に作るためのView
 class Index(ListView):
     # 一覧するモデルを指定 -> `object_list`で取得可能
@@ -11,3 +11,12 @@ class Index(ListView):
 class Detail(DetailView):
     # 詳細表示するモデルを指定 -> `object`で取得可能
     model = Post
+    
+
+
+# CreateViewは新規作成画面を簡単に作るためのView
+class Create(CreateView):
+    model = Post
+
+    # 編集対象にするフィールド
+    fields = ["title", "body"]
