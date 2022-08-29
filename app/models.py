b562f9344c1141e42f3dd5d2ff8da3cd19584e27
unohayato <mamidori1p1p@gmail.com>
2022-08-29 21:21:05 +0900

    models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+
+
+
+class Post(models.Model):
+    created = models.DateTimeField(
+        auto_now_add=True,
+        editable=False,
+        blank=False,
+        null=False)
+    
+    updated = models.DateTimeField(
+        auto_now=True,
+        editable=False,
+        blank=False,
+        null=False)
+        
+    title = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False)
+        
+    body = models.TextField(
+        blank=True,
+        null=False)
+          
+
+    def __str__(self):
+        return self.title
