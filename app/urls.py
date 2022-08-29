e35e26fbbf42ef7cc928366be909788c6705a96b
unohayato <mamidori1p1p@gmail.com>
2022-08-29 21:39:02 +0900

    post_form.html

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name="index"),
-    
     # <pk>にPostのIDを渡すと表示される。
     path('detail/<pk>/', views.Detail.as_view(), name="detail"),
+    path('create/', views.Create.as_view(), name="create"),
 ]
