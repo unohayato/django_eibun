fdeca276a12e4c7d2d1e06f4ceb33ecbf6e091c2
unohayato <mamidori1p1p@gmail.com>
2022-08-29 21:24:09 +0900

    admin
    register

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post
-# Register your models here.
-
-admin.site.register('Post')
+from . import models
 
 
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
